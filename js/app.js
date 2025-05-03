f88d57fd740ca97e40fdafc28568ee2202637c71
C4rbon <120744803+SerafimLupan@users.noreply.github.com>
2025-05-03 20:24:44 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
+//IP finder
+document.getElementById("logBtn").addEventListener("click", async () => {
+    const response = await fetch("https://api.ipify.org?format=json");
+    const data = await response.json();
+
+    const payload = {
+        ip: data.ip,
+        timestamp: new Date().toISOString(),
+        userAgent: navigator.userAgent,
+        page: window.location.href
+    };
+
+    fetch("http://localhost:3000/log-visit", { // IMPORTANT: url corect spre server
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+    });
+});
+
+
+// Scroll effect
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry)
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
